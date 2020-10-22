06ca482354f7c44bc814335b63ab8bf82888c25c
M.S. Allahkaram <msinamsina@gmail.com>
2020-10-22 17:02:22 +0330

    adding parser

--- sendingGmail.py
+++ sendingGmail.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Oct  4 14:09:32 2020
 
-@author: 98936
+@author: Mohammad sina Allahkaram
 """
 import smtplib, ssl
 import time
@@ -10,78 +10,77 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import argparse
+import getpass 
 
+parser = argparse.ArgumentParser()
 
+parser.add_argument("email", help="enter you Gmail account")
+parser.add_argument("contacts", help="path to csv file which contact emails are listed there")
+parser.add_argument("--body", help="path to E-mail text file")
+parser.add_argument("--html", help="path to E-mail Html file")
+parser.add_argument("--attachment", help="path to pdf file that you want to attach to your E-mail")
 
+arg=parser.parse_args()
+
+sender_email = arg.email
+receivers_email = []
+with open(arg.contacts) as contactFile:
+    for email in contactFile:
+        receivers_email.append(email.split('\n')[0])
+
+port = 465  # For SSL
+password = getpass.getpass("Enter you email password  :") 
 
 subject = "An email with attachment from Python"
 
+message = MIMEMultipart("alternative")
+message["Subject"] = subject
+message["From"] = sender_email
 
+if arg.body:
+    with open(arg.body) as f:
+        body = f.read()
+    message.attach(MIMEText(body, "plain"))
+    print(body)
 
-port = 465  # For SSL
-password = "your password"
-gmail="your@gmail.com"
+if arg.html:
+    with open(arg.html) as f:
+        html = f.read()
+    message.attach(MIMEText(html, "html"))
+    print(html)
 
+if arg.attachment:
 
-sender_email = gmail
-receivers_email = []
+    filename = arg.attachment
 
-receivers_email.append("contact1@gmail.com")
-receivers_email.append("contact2@gmail.com")
-receivers_email.append("contact3@gmail.com")
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
 
-message = MIMEMultipart()
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+    # Add attachment to message and convert message to string
+    message.attach(part)
 
-
-
-body = "This is an email with attachment sent from Python"
-
-html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       How are you?<br>
-       <a href="http://www.realpython.com">Real Python</a> 
-       has many great tutorials.
-    </p>
-  </body>
-</html>
-"""
-
-
-message.attach(MIMEText(body, "plain"))
-
-message.attach(MIMEText(html, "html"))
-
-filename = "document.pdf"  # In same directory as script
-
-# Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
-
-# Encode file in ASCII characters to send by email    
-encoders.encode_base64(part)
-
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
-
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
-
-# Create a secure SSL context
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(gmail, password)
-    # TODO: Send email here
-    server.sendmail(sender_email, receivers_email[0], text)
-    time.sleep(10)
-    print('message send to '+receivers_email[0])
+for receiver in receivers_email:
+    message["To"] = receiver    
+    text = message.as_string()
+    
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver, text)
+        time.sleep(10)
+        print('message send to '+receiver)
