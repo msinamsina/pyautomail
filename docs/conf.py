349bfa5a688ebb46c8d2e496adcb8925d6864f81
msina <msinamsina@gmail.com>
2023-07-26 12:51:07 -0500

    modify docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,25 +14,23 @@ release = '1.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+# html_theme = 'alabaster'
 html_static_path = ['_static']
-
-extensions = [
-    # 'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-]
 
 master_doc = 'index'
 
-# html_theme = "sphinx_rtd_theme"
+
