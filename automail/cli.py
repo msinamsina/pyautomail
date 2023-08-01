f3a5f4007f28ee4de1887bcebaa257a0720bbea2
msina <msinamsina@gmail.com>
2023-08-01 17:22:24 -0500

    init command is added to cli

--- automail/cli.py
+++ automail/cli.py
@@ -3,6 +3,8 @@ from typing import Optional
 import typer
 import shutil
 from automail.storage import get_session, create_tables
+import time
+import os
 
 
 app = typer.Typer()
@@ -38,13 +40,10 @@ def init(
         prompt="Where do you want to initialize the automail project?",
     ),
 ) -> None:
-    """Initialize the to-do database."""
+    """Initialize the mail database."""
     from automail.storage import util
     from automail import __app_name__
 
-    typer.echo(f"Initializing {__app_name__} database...")
-
-    # db_path = shutil.os.path.abspath(db_path)
     if not shutil.os.path.exists(db_path):
         shutil.os.mkdir(db_path)
     else:
@@ -52,14 +51,23 @@ def init(
         # delete the folder if it exists
         if typer.confirm("Do you want to delete it?", default=False):
             shutil.rmtree(db_path)
+            print("Deleting...")
+            time.sleep(0.5)
             shutil.os.mkdir(db_path)
+
         else:
             typer.echo("Aborted!")
-            raise typer.Exit(code=1)
-    shutil.os.chdir(db_path)
+            raise typer.Exit(code=0)
+
+    typer.echo(f"Initializing {__app_name__} database...")
+    os.chdir(db_path)
     session, engin = get_session()
     create_tables(engin)
     typer.echo("Done!")
+    os.chdir("../")
+
+    session.close()
+    engin.dispose()
     return
 
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 from typer.testing import CliRunner
-
+import shutil
+import os
 from automail import __app_name__, __version__, cli
 
 runner = CliRunner()
@@ -11,13 +12,62 @@ def test_version():
     assert f"{__app_name__} v{__version__}\n" in result.stdout
 
 
-def test_init():
+def test_init_1():
+    # check if the init command is working
     result = runner.invoke(cli.app, ["init"])
-    assert result.exit_code == 1
+    assert result.exit_code == 0
     assert "Where do you want to initialize the automail project? [./]: " in result.stdout
+
+
+def test_init_2():
     # enter a path that does not exist
+    shutil.rmtree("test", ignore_errors=True)
     result = runner.invoke(cli.app, ["init"], input="test\n")
-    assert result.exit_code == 1
+    assert result.exit_code == 0
+    assert "Where do you want to initialize the automail project? [./]: " in result.stdout
+    assert "Initializing automail database..." in result.stdout
+    assert "Done!" in result.stdout
+    assert os.path.exists("test") is True
+    assert os.path.exists("test/mail.db") is True
+
+
+def test_init_3():
+    # enter a path that already exists and choose not to delete it
+    result = runner.invoke(cli.app, ["init"], input="test\nn\n")
+    assert result.exit_code == 0
+    assert "Where do you want to initialize the automail project? [./]: " in result.stdout
     assert "Directory test already exists." in result.stdout
     assert "Do you want to delete it? [y/N]: " in result.stdout
-    # press enter to abort
+    assert "Initializing automail database..." not in result.stdout
+    assert "Aborted!" in result.stdout
+
+
+def _helper_init(result):
+    assert result.exit_code == 0
+    assert "Directory test already exists." in result.stdout
+    assert "Do you want to delete it? [y/N]: " in result.stdout
+    assert "Deleting..." in result.stdout
+    assert "Initializing automail database..." in result.stdout
+    assert "Done!" in result.stdout
+
+    assert os.path.exists("test") is True
+    assert os.path.exists("test/mail.db") is True
+
+
+def test_init_4():
+    # enter a path that already exists and choose to delete it
+    result = runner.invoke(cli.app, ["init"], input="test\ny\n")
+    _helper_init(result)
+    assert "Where do you want to initialize the automail project? [./]: " in result.stdout
+
+
+def test_init_5():
+
+    # enter a path that already exists and choose to delete it
+    result = runner.invoke(cli.app, ["init", "--db-path", "test"], input="y\n")
+    _helper_init(result)
+
+
+def test_init_6():
+    result = runner.invoke(cli.app, ["init", "-db", "test"], input="y\n")
+    _helper_init(result)
