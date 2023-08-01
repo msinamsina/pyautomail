b3756d0e96de2738f8be2b6951be19a5951b769f
msina <msinamsina@gmail.com>
2023-08-01 15:13:15 -0500

    $ git commit -m

--- automail/storage/util.py
+++ automail/storage/util.py
@@ -1,5 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
 
